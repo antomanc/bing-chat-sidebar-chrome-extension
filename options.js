c1727c9a1d73b75d0e45a599b85f3b0b84112303
Mike Orozco <me@mikeorozco.dev>
2023-05-10 00:21:28 -0500

    More static callbacks to improve performance

--- options.js
+++ options.js
@@ -2,26 +2,30 @@ const popupCheckBox = document.getElementById("popupActive");
 const darkModeCheckBox = document.getElementById("darkModeActive");
 
 // set the popup checkbox to the value stored in chrome storage or to true if it doesn't exist
-chrome.storage.sync.get(["popupActive"], function (result) {
+const popupActiveCallback = (result) => {
   popupCheckBox.checked = result.popupActive ?? true;
-});
+};
+chrome.storage.sync.get(["popupActive"], popupActiveCallback);
 
 // set the dark mode checkbox to the value stored in chrome storage or to false if it doesn't exist
-chrome.storage.sync.get(["darkModeActive"], function (result) {
+const darkModeActiveCallback = (result) => {
   darkModeCheckBox.checked = result.darkModeActive ?? false;
-});
+};
+chrome.storage.sync.get(["darkModeActive"], darkModeActiveCallback);
 
 // add an onchange event listener to the popup checkbox
-popupCheckBox.addEventListener("change", function () {
+const poupupCheckboxChangeEventHandler = () => {
   const value = popupCheckBox.checked;
   setPopup(value);
-});
+};
+popupCheckBox.addEventListener("change", poupupCheckboxChangeEventHandler);
 
 // add an onchange event listener to the dark mode checkbox
-darkModeCheckBox.addEventListener("change", function () {
+const darkModeCheckboxChangeEventHandler = () => {
   const value = darkModeCheckBox.checked;
   setDarkMode(value);
-});
+};
+darkModeCheckBox.addEventListener("change", darkModeCheckboxChangeEventHandler);
 
 // function to set the dark mode value
 const setDarkMode = (value) => {
